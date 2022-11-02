1f6b233b2acc8eb456bd6c95bcc823cecfbee492
audreybrown <audreybrown@localhost>
2022-11-02 15:16:21 -0500

    hi

---
+++ src/studio7/Rectangle.java
@@ -0,0 +1,52 @@
+package studio7;
+
+public class Rectangle {
+
+	private int length;
+
+	private int width;
+
+	// public = other classes can access it, private = only the class can access it
+	public Rectangle(int length, int width) {
+
+		// this refers to the current
+		this.length= (length);
+		this.width = (width);
+
+	}
+
+	/** Declaring the area
+	 * 
+	 * @return
+	 */
+	public int Area() {
+
+		return 	length*width;
+
+
+	}
+
+	public int Perimeter() {	
+
+		return length+length+width+width;
+
+
+	}
+
+
+	public boolean WhetherItsASquare()
+
+
+return 
+		
+		length=width;
+
+}
+public int ComparingToOtherRectangles()
+
+
+}
+
+}
+
+
